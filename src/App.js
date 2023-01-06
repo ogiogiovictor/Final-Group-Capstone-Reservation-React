9b3e6241d3a6c993c840d66d7605cdf7f41afa18
Fernando Silva <fernando.silvabr66@gmail.com>
2023-01-06 20:41:52 -0300

    Initial API (BACKEND) connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Motorcycles from './components/motorcycles';
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://localhost:3000/api/v1/motorcycles";
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
+
+  const [motorcycles, setMotorcycles] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setMotorcycles(items);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello - API connection testing</h1>
+      <Motorcycles motorcycles={motorcycles} />
     </div>
   );
 }
