123cabc06b8017b6becce18c8f078176674d99d2
OgiogioVictor <ogiogiovictor@gmail.com>
2023-01-07 15:15:33 -0600

    site setup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Slider from './components/frontpage/mainpage';
 import './App.css';
-import axios from 'axios';
-import Motorcycles from './components/motorcycles';
-import { useEffect, useState } from 'react';
-
-const API_URL = "http://localhost:3000/api/v1/motorcycles";
-
-function getAPIData() {
-  return axios.get(API_URL).then((response) => response.data)
-}
 
 function App() {
-
-  const [motorcycles, setMotorcycles] = useState([]);
-
-  useEffect(() => {
-    let mounted = true;
-    getAPIData().then((items) => {
-      if (mounted) {
-        setMotorcycles(items);
-      }
-    });
-    return () => (mounted = false);
-  }, []);
-
   return (
     <div className="App">
-      <h1>Hello - API connection testing</h1>
-      <Motorcycles motorcycles={motorcycles} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Slider />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/frontpage/mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainPage = () => {
+
+    return (
+        <div>
+           <div className="mainTitle">The New Vespa Tridient</div>
+           <center><button className="btn btn-lg btn-vespa">Learn More</button></center>
+        </div>
+    )
+
+}
+
+export default MainPage;
