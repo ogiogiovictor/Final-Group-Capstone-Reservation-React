5176ed7cb3bcb0a5ef90a351254e623b3e2d30c6
OgiogioVictor <ogiogiovictor@gmail.com>
2023-01-08 05:37:33 -0600

    site structure with navigation implementation

--- src/components/layouts/navigation.js
+++ src/components/layouts/navigation.js
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import logo from '../../vespa_logo.jpg';
 import '../css/navigation.css';
 
 const Navigation = () => {
 
     return (
         <div className="col-sm-2 border">
-         Navigation Area
+         <img src={ logo } class="logo"/>
+         
+         <ul>
+            <li>MODELS</li>
+            <li>LIFESTYLE</li>
+            <li>SHOP </li>
+            <li>TEST DRIVE</li>
+         </ul>
         </div>
     )
 
