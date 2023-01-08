f59b46e0fc194fa75736b876ef72064c673f726b
OgiogioVictor <ogiogiovictor@gmail.com>
2023-01-08 11:00:16 -0600

    updated content area

--- src/components/layouts/content.js
+++ src/components/layouts/content.js
@@ -5,8 +5,9 @@ import '../css/content.css';
 const Content = () => {
     
         return (
-            <div  className="col-sm-9">
-            Content Area
+            <div  className="col-sm-9 content-area">
+             <h2>LATEST MODELS</h2>
+             <div>Choose from our exquisite designs</div>
             </div>
         )
     
