7a8e98dc35921449b1fa8c1b3aad87322fa19d86
OgiogioVictor <ogiogiovictor@gmail.com>
2023-01-08 08:09:51 -0600

    added footer

--- src/components/layouts/navigation.js
+++ src/components/layouts/navigation.js
@@ -11,11 +11,26 @@ const Navigation = () => {
          <img src={ logo } class="logo"/>
          
          <ul>
-            <li><NavLink to="/models" class="active">MODELS</NavLink></li>
+            <li><NavLink to="/models" className="active">MODELS</NavLink></li>
             <li><NavLink to="/lifesytle">LIFESTYLE</NavLink></li>
             <li><NavLink to="/shop">SHOP</NavLink> </li>
             <li><NavLink to="/drive">TEST DRIVE </NavLink></li>
          </ul>
+
+         <footer class="footera-area">
+            <div>
+            <a href=""><i className="fa fa-facebook" aria-hidden="true"></i></a>
+            <a href=""><i className="fa fa-twitter"></i></a>
+            <a href=""><i className="fa fa-linkedin"></i></a>
+            <a href=""><i className="fa fa-youtube"></i></a>
+            </div>
+            <div>
+            <small className="footer-small">© 2021 Piaggio & C. S.p.A. - All rights reserved</small>
+            </div>
+           
+         </footer>
+       
+         
 
         </div>
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "font-awesome/css/font-awesome.min.css";
 
 import App from './App';
 
