95c341a0caf06c4089ac196953384fba30c030b6
Omachi John <omachi.john@yahoo.com>
2023-01-25 22:00:13 +0100

    Authenticate user login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Slider from './components/frontpage/mainpage';
-import CarModels from './components/models/carmodels';
-import TestRide from './components/models/testride';
-
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 import './App.css';
+import { fetchUser } from './features/user/userSlice';
+import { fetchReservatins } from './features/reservations/reservationSlice';
+import SideBar from './components/SideBar';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.users);
+
+  const isUser = () => {
+    if (!currentUser) {
+      return (
+        <LoginPage />
+      );
+    }
+    return (
+      <>
+        <SideBar />
+        <Outlet />
+      </>
+    );
+  };
+
+  React.useEffect(() => {
+    dispatch(fetchUser());
+    dispatch(fetchReservatins());
+  }, [dispatch]);
+
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Slider />} />
-          <Route path="/home" element={<CarModels />} />
-          <Route path="/test_ride" element={<TestRide />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      {isUser()}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "font-awesome/css/font-awesome.min.css";
-
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import store from './app/store';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import HomePage from './components/HomePage';
+import CarDeletePage from './components/CarDeletePage';
+import MyReservationsPage from './components/MyReservationsPage';
+import SingleCarPage from './components/SingleCarPage';
+import Reserve from './components/Reserve';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<App />}>
+            <Route element={<HomePage />} index />
+            <Route element={<SingleCarPage />} path="cars/:id" />
+            <Route element={<MyReservationsPage />} path="reservations" />
+            <Route element={<Reserve />} path="/reserve" />
+            <Route element={<CarDeletePage />} path="/delete" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
